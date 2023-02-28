81bad8d5cf5c26e4de4e35b359e54d21cae6a2d3
congle98 <79907265+congle98@users.noreply.github.com>
2023-02-28 22:40:38 +0700

    oke

--- src/main/java/congdz98/classroom_be/controller/StudentController.java
+++ src/main/java/congdz98/classroom_be/controller/StudentController.java
@@ -12,6 +12,6 @@ public class StudentController {
 
     @GetMapping()
     public ResponseEntity<Person> getAll(){
-        return ResponseEntity.ok(new Person().setName("congdz199fsdf8hihihffuhu1111").setAge(11));
+        return ResponseEntity.ok(new Person().setName("congdz199fsdf8hihihffuhu112321311").setAge(11));
     }
 }
